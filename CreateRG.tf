b4860871d316e453d91dcd5a7f278104dfa24384
BenjaminVanassche <benjaminvanassche01@gmail.com>
2022-08-19 09:40:27 +0200

    Update CreateRG.tf

--- CreateRG.tf
+++ CreateRG.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   
   subscription_id = "e42a170e-47e2-4a94-b2b1-27a3fb2a8411"
   client_id       = "2fce7617-9e1a-4112-aa4f-2f6e47ef5d53"
-  client_secret   = "Z3K8Q~_PzWZsOPSKIQ00envWrapGC4kKRCmGzb9Y"
+  client_secret   = Z3K8Q~_PzWZsOPSKIQ00envWrapGC4kKRCmGzb9Y
   tenant_id       = "c0b589b7-ecf2-456f-a711-8437a24463b3"
 }
 
